67684f08bf3e095dc6cb631e7e2a2c19a97596b7
Wells-Wait <147658484+Wells-Wait@users.noreply.github.com>
2026-08-29 14:26:07 -0600

    Update shared memory includes to version 3.5

--- include/sharedMemory.h
+++ include/sharedMemory.h
@@ -8,8 +8,8 @@
 #include <boost/interprocess/sync/interprocess_sharable_mutex.hpp>
 #include <boost/interprocess/sync/sharable_lock.hpp>
 #include <boost/interprocess/sync/scoped_lock.hpp>
-#include "MF26/v2/vehicle.pb.h"
-#include "MF26/v2/control.pb.h"
+#include "MF26/v3/vehicle.pb.h"
+#include "MF26/v3/control.pb.h"
 enum class SharedMemoryManagerMode{
     VCU,
     SCREEN,
@@ -39,7 +39,7 @@ struct VehicleStateSharedMemory{
     uint8_t readerCounterVCULast;
     uint8_t readerCounterScreenLast;
     VehicleStateSharedMemoryPacket* sharedMemory;
-    MF26::v2::VehicleState *vehicleState;
+    MF26::v3::VehicleState *vehicleState;
 
 };
 
@@ -59,7 +59,7 @@ struct VCUCommandSharedMemory{
     uint8_t readerCounterServerLast;
     uint8_t readerCounterScreenLast;
     VCUCommandSharedMemoryPacket* sharedMemory;
-    MF26::v2::VCUCommand *vcuCommand;
+    MF26::v3::VCUCommand *vcuCommand;
 
 };
 
